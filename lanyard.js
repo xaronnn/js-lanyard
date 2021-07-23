1752f04025925d1f9f8db13e0f59857d180920cb
Uğur <ugurpekesensteam@gmail.com>
2021-07-23 03:44:10 +0300

    Create lanyard.js

---
+++ lanyard.js
@@ -0,0 +1,42 @@
+"use strict";
+
+class Lanyard {
+  #version = "1.0";
+
+  constructor(id) {
+    if(!id) return;
+    this.id = id;
+    this.lanyard = new WebSocket("wss://api.lanyard.rest/socket");
+    this.opCodes = {
+      INFO: 0,
+      HELLO: 1,
+      INIT: 2,
+      HEARTBEAT: 3,
+    };
+  }
+  
+  listen(x) {
+    this.lanyard.onmessage = ({data}) => {
+      const parsedData = JSON.parse(data);
+      
+      switch(parsedData.op) {
+        case this.opCodes.HELLO:
+          this.lanyard.send(JSON.stringify({
+            op: this.opCodes.INIT,
+            d: {
+              subscribe_to_id: this.id
+            }
+          }));
+          setInterval(() => {
+            lanyard.send(JSON.stringify({
+              op: this.opCodes.HEARTBEAT
+            }))
+          }, parsedData.d.heartbeat_interval);
+          break;
+        case this.opCodes.INFO:
+          x(parsedData.d);
+          break;
+      }
+    }
+  }
+}
